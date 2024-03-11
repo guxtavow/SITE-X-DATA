4db6d0d297c96a1aaa82500c8f9e68a21dfb348b
guxtavow <aquinoavilag@gmail.com>
2024-03-11 16:41:07 -0300

    feat banner

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -1,12 +1,28 @@
 import './Banner.css'
+import {useState, useEffect} from 'react'
+
 
 export default function Banner(){
+    const [acao,setAcao] = useState(false)
+
+    useEffect(() =>{
+    const temp = setTimeout(() => setAcao(true),1000)
+    return () => clearTimeout(temp)
+},[])
+
     return(
         <>
-        <div className="container" id="banner">
-            teste
+            <div className='container-banner' id='banner'>
+                <div className={acao ? "evento-off evento": "evento-off"}>
+                    <h1 className='Title'>
+                        <b>X-DATA</b><br />
+                    </h1>
+                    <h3 className='subtitulo'>
+                        <b>Quando você liga, o seu futuro começa </b>
+                    </h3>
+                </div>
 
-        </div>
+            </div>
         </>
     )
 }
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,33 +1,45 @@
 import logoX from './logo-X1.png'
-import logoX1 from './logo-X.png'
 import './Header.css'
 
 
 export default function Header(){
     return(
         <header>
-            <nav class="navbar navbar-expand-lg body-tertiary" style={{backgroundColor: 'black'}}>
+            <center>
+            <nav class="navbar navbar-expand-lg body-tertiary" style={{backgroundColor: 'transparent'}}>
             <div class="container-fluid">
-                <img src={logoX} className="LogoX" alt="Xlogo" width="100" height="50" />
-                <a class="navbar-brand" href="#" style={{color:'#901090', fontSize:'30px'}}><b><i>X-Data</i></b></a>
+                <img src={logoX} className="LogoX" alt="Xlogo" width="100" height="50"/>
+                <a class="navbar-brand" href="#" style={{color:'blueviolet', fontSize:'40px'}}><b><i>X-DATA</i></b></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#" style={{color:'#900090'}}><b>Inicio</b></a>
+                    <a class="nav-link active" aria-current="page" href="#" ><b>Inicio</b></a>
                     </li>
+
                     <li class="nav-item">
-                    <a class="nav-link" href="#" style={{color:'#900090'}}><b>Curiosidades</b></a>
+                    <a class="nav-link" href="#"><b>Especificações</b></a>
                     </li>   
+
                     <li class="nav-item">
-                    <a class="nav-link" href="#" style={{color:'#900090'}}><b>Quem somos</b></a>
+                    <a class="nav-link" href="#"><b>Valores</b></a>
                     </li>
+
+                    <li class="nav-item">
+                    <a class="nav-link" href="#"><b>Quem somos</b></a>
+                    </li>
+
+                    <li class="nav-item">
+                    <a class="nav-link" href="#"><b>Fale Conosco</b></a>
+                    </li>
+
                 </ul>
                 </div>
             </div>
             </nav>
+            </center>
         </header>
 
     )
