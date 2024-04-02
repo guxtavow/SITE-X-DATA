53fc29d4cb579fb1d07108e0b566449fe77614a8
guxtavow <aquinoavilag@gmail.com>
2024-04-02 13:45:28 -0300

    feat footer + fix visual

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header.js';
 import Banner from './components/Banner/Banner.js';
 import Laydowns from './components/laydown/Laydown.js';
+import Footer from './components/Footer/Footer.js';
 
 
 function App() {
   return (
     <div className="App">
-      {/* ----------------------------- HEADER ----------------------------- */}
+      {/* ----------------------------- HEADER - PAGE ----------------------------- */}
       <Header />
-      {/* ---------------------------------- MAIN ---------------------------------- */}
+      {/* ---------------------------------- CONTEUDO - MAIN PAGE ---------------------------------- */}
       <Banner />
-      
       <Laydowns />
-      
+      {/* ---------------------------------- FOOTER - MAIN PAGE ---------------------------------- */}
+      <Footer />
+
 
     </div>
   );
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,29 @@
+import './Footer.css'
+import Linkedin from './linkedin.png'
+import github from './github.png'
+
+export default function Footer(){
+    
+return(
+    <>
+    <footer id='foot' className='HomeFooter'>
+        <div className='textoFooter'>
+            <h7>
+            Copyright © 2024 X-DATA. Todos os direitos reservados. Todas as marcas registradas mencionadas são propriedade
+            de seus respectivos proprietários. Este site é protegido por leis de direitos autorais e outras leis de propriedade intelectual.
+            O uso não autorizado deste site pode violar leis de direitos autorais, marcas registradas e outras regulamentações.
+            Ao acessar este site, você concorda com os termos e condições de uso.
+            </h7>
+            <br />
+        <div className='Sociais'>
+            <a href='https://www.linkedin.com/in/gustavodeaquinoavila/' rel="noreferrer" target='_blank' id='linkedin'><img src={Linkedin} alt='Linke' width='50px' id='link'></img></a>
+            <a href='https://github.com/guxtavow' rel="noreferrer" target='_blank' id='GitHub'><img src={github} alt='Git' width='50px' id='github'></img></a>
+
+
+        </div>
+        </div>
+    </footer>
+    </>
+)
+
+}
